6058d05ddc924436cd4b0a8dfb6fbd532114fb76
Min-Ho Lee <minho.lee0716@gmail.com>
2021-02-20 03:01:20 +0900

    Add: Test Login API

--- user/views.py
+++ user/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.views import View
+from django.http import JsonResponse, HttpResponse
 
-# Create your views here.
+import json
+
+class SignInView(View):
+    def post(self, request):
+        payload = json.loads(request.body)
+        phone_number = payload.get('phone_number', None)
+        password = payload.get('password', None)
+
+        if phone_number is None or password is None:
+            return HttpResponse(400)
+
+        if phone_number == '01012345678' and password == 'a123':
+            return HttpResponse()
+        else:
+            return HttpResponse(401
