dff2cf209f0c6418c4ca3af189a7323c9fc49c4c
Min-Ho Lee <minho.lee0716@gmail.com>
2021-05-26 23:57:57 +0900

    Add: User Model in user.models

--- user/models.py
+++ user/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=255, unique=True)
+    password = models.CharField(max_length=300)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'users'
