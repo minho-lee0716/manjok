931478014e66ef992585e2b68fcc267870e850cc
Min-Ho Lee <minho.lee0716@gmail.com>
2021-06-02 23:58:08 +0900

    Add: talent field in User Model in user app

--- user/models.py
+++ user/models.py
@@ -4,6 +4,7 @@ class User(models.Model):
     name = models.CharField(max_length=50)
     email = models.EmailField(max_length=255, unique=True)
     password = models.CharField(max_length=300)
+    talents = models.IntegerField()
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -11,3 +12,7 @@ class User(models.Model):
 
     class Meta:
         db_table = 'users'
+
+
+class UserTalentLogs(models.Model):
+    user_id = models.ForeignKey(User)
