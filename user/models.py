5604db876f97f9f3eadec62130a0e1775b77c51c
Min-Ho Lee <minho.lee0716@gmail.com>
2021-07-10 20:19:00 +0900

    유저 모델 테스트

--- user/models.py
+++ user/models.py
@@ -22,10 +22,17 @@ class UserTalentLogs(models.Model):
         db_table = 'user_talent_logs'
 
 class Board(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField()
+    title = models.CharField(max_length=200, verbose_name="제목")
+    content = models.TextField(verbose_name="내용")
+    writer  = models.ForeignKey('account.Account', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def getname(self):
+        return Account.objects.filter(name=self)
 
     class Meta:
         db_table = 'boards'
